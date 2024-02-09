f003d414dd7593bf2849e0d8b3016e6b5610b37f
morganb75 <morgan.bleunven@maif.fr>
2024-02-09 22:21:19 +0100

    modif clavier

---
+++ fonctions.js
@@ -0,0 +1,23 @@
+function sum(a,b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    return (a+b);
+}
+
+function minus(a,b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    return (a-b);
+}
+
+function multiply(a,b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    return (a*b);
+}
+
+function divide(a,b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    return (a/b);
+}
