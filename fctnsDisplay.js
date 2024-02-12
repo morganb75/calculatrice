d05de6dac9a8d417168e27d7f8512285c5b9dd9e
morganb <morgan.bleunven@maif.fr>
2024-02-12 15:40:40 +0100

    calculs ok sans gestion d'exception

--- fctnsDisplay.js
+++ fctnsDisplay.js
@@ -3,7 +3,7 @@ dans l'input de display*/
 
 function displayValue() {
     let buttons = document.querySelectorAll(".btn, .btn-l-r");
-buttons.forEach(function(button) {
+buttons.forEach((button) => {
         button.addEventListener("click",() => {
                 let value = button.textContent;
                 // console.log(value);
@@ -21,8 +21,33 @@ buttons.forEach(function(button) {
 
 function processingInput(operation) {
     let eqButton = document.getElementById("btn-eq");
+    let inputDisplay = document.getElementById("input-value");
+    let resDisplay = document.getElementById("input-result");
     eqButton.addEventListener("click",() => {
         operation = document.getElementById("input-value").value;
-        console.log(operation);
+        let num1;
+        let num2;
+        let operande;
+        let result = operation.split(/([+\-x\/])/);
+        let resFinal
+        num1 = result[0];
+        num2 = result[2];
+        operande = result[1];
+        console.log(result);
+        console.log(num1+" "+num2+" "+operande);
+
+        switch(operande) {
+            case '+': resFinal = sum(num1,num2);
+            break;
+            case '-': resFinal = minus(num1,num2);
+            break;
+            case 'x': resFinal = multiply(num1,num2);
+            break;
+            case '/': resFinal = divide(num1,num2);
+            break;
+        }
+        inputDisplay.value = inputDisplay.value +"=";
+        resDisplay.value = resFinal;
+
     })
 }
--- fctnsOperandes.js
+++ fctnsOperandes.js
@@ -1,23 +1,23 @@
 function sum(a,b) {
-    a = parseInt(a);
-    b = parseInt(b);
+    a = parseFloat(a);
+    b = parseFloat(b);
     return (a+b);
 }
 
 function minus(a,b) {
-    a = parseInt(a);
-    b = parseInt(b);
+    a = parseFloat(a);
+    b = parseFloat(b);
     return (a-b);
 }
 
 function multiply(a,b) {
-    a = parseInt(a);
-    b = parseInt(b);
+    a = parseFloat(a);
+    b = parseFloat(b);
     return (a*b);
 }
 
 function divide(a,b) {
-    a = parseInt(a);
-    b = parseInt(b);
+    a = parseFloat(a);
+    b = parseFloat(b);
     return (a/b);
 }
